7dab164efd100af934bbc5d85814b3b6a3f80303
Dylan Dreyer <dyldreyer04@gmail.com>
2016-05-15 01:46:34 -0700

    first day

--- client/client.py
+++ client/client.py
@@ -1,6 +1,7 @@
 import curses
 from curses import wrapper
 import json
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 import urllib.request
 
 def main(stdscr):
@@ -13,20 +14,25 @@ def main(stdscr):
     curses.curs_set(2)
     while True:
         cursorPos = stdscr.getyx()
-        key = stdscr.getch()
-        
-        
-        newDirection = {"direction":keyDir(key)}
+#        key = stdscr.getch()
+ #       if key == ord('q'):
+  #          exit(stdscr)
+        url = "http://52.34.125.56/grid"
+        newDirection = {"direction":0}
         params = json.dumps(newDirection).encode('utf8')
-        req = urllib.request.Request
+        req = urllib.request.Request(url, data=params)
+        response = urllib.request.urlopen(req)
+#        print(response.read().decode('utf8'))
 #        if key in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 'q']:
 #           if key == q:
 #                exit(stdscr)
 #            elif key == KEY_LEFT:
 #                stdscr.move(
-#        stdscr.refresh()
+        stdscr.refresh()
 
-def displaySoldier(stdsc,y,x):
+def request(url, dic):
+
+def displaySoldier(stdscr,y,x):
     stdscr.addch(y,x,'#')
 
 def keyDir(key):
