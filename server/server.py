d871b058962cbebd1845f08b5dd98513c89d8407
thabaptiser <baptiste.vauthey@gmail.com>
2016-05-15 21:33:33 -0700

    small fixes

--- server/server.py
+++ server/server.py
@@ -3,58 +3,64 @@ import json
 from collections import namedtuple
 import random
 
+max_id = 0
+units = {}
+
 class Unit():
-    def __init__(self, x, y):
+    def __init__(self, x, y, id):
         self.x = x
         self.y = y
+        self.id = id
 
-unit = Unit(0,0)
+def add_unit():
+    units[max_id] = Unit(0,0, max_id)
+    max_id += 1
 
-def get_num_at_end_of_branch(branch):
-    if branch % 2 == 0:
-        x = branch//2
-        return (x) * (x+1)
-    else:
-        x = branch//2
-        return (x) * (x+1) + (branch+1)//2
+#def get_num_at_end_of_branch(branch):
+#    if branch % 2 == 0:
+#        x = branch//2
+#        return (x) * (x+1)
+#    else:
+#        x = branch//2
+#        return (x) * (x+1) + (branch+1)//2
 
-def get_curve_position(x, y):
-    """
-    given an x and y, gives you the integer location on the space filling curve
-    """
-    ya = int(abs(y))
-    xa = int(abs(x))
-    horizontal = False
-    vertical = False
-    if xa >= ya:
-        vertical = True
-        if x > 0:
-            branch = 4*x-2
-            if y < 0:
-                if ya == x:
-                    vertical = False
-                    horizontal = True
-                branch += 1
-        else:
-            branch = 4*xa
-    elif ya > xa:
-        horizontal = True
-        if y > 0:
-            branch = 4*y-3
-        else:
-            branch = 4*ya-1
-    last_corner = get_num_at_end_of_branch(branch)
-    if horizontal and y > 0:
-        total_num = last_corner + x + y - 1
-    elif horizontal and y < 0:
-        total_num = last_corner + ya - x
-    elif vertical and x > 0:
-        total_num = last_corner + (x-y)
-    elif vertical and x < 0:
-        total_num = last_corner + xa + y
-    elif x == y == 0:
-        return 0
-    return total_num
+#def get_curve_position(x, y):
+#    """
+#    given an x and y, gives you the integer location on the space filling curve
+#    """
+#    ya = int(abs(y))
+#    xa = int(abs(x))
+#    horizontal = False
+#    vertical = False
+#    if xa >= ya:
+#        vertical = True
+#        if x > 0:
+#            branch = 4*x-2
+#            if y < 0:
+#                if ya == x:
+#                    vertical = False
+#                    horizontal = True
+#                branch += 1
+#        else:
+#            branch = 4*xa
+#    elif ya > xa:
+#        horizontal = True
+#        if y > 0:
+#            branch = 4*y-3
+#        else:
+#            branch = 4*ya-1
+#    last_corner = get_num_at_end_of_branch(branch)
+#    if horizontal and y > 0:
+#        total_num = last_corner + x + y - 1
+#    elif horizontal and y < 0:
+#        total_num = last_corner + ya - x
+#    elif vertical and x > 0:
+#        total_num = last_corner + (x-y)
+#    elif vertical and x < 0:
+#        total_num = last_corner + xa + y
+#    elif x == y == 0:
+#        return 0
+#    return total_num
 
 def get_x_y_position(curve_pos):
     """
@@ -81,21 +87,21 @@ class MoveUnitResource:
         """Handles post requests"""
         req_json = json.loads(req.stream.read().decode('utf-8'))
         direction = req_json['direction']
+        id = req_json['id']
         if direction == 0:
-            unit.y += 1
+            units[id].y += 1
         elif direction == 1:
-            unit.x += 1
+            units[id].x += 1
         elif direction == 2:
-            unit.y -= 1
+            units[id].y -= 1
         elif direction == 3:
-            unit.x -= 1
-        #unit_id = req_json['unit_id']
+            units[id].x -= 1
         resp.status = falcon.HTTP_200
 
 
 class GetGridResource:
     def on_get(self, req, resp):
-        resp.body = json.dumps({'units': [(unit.x, unit.y)]})
+        resp.body = json.dumps({'units': {id: unit.x, unit.y for unit in units.values()}})
 
 api = falcon.API()
 api.add_route('/unit/move', MoveUnitResource())
