71cae1053922dcfb3fbee07f5cdd10505042c5f0
thabaptiser <baptiste.vauthey@gmail.com>
2016-05-15 18:37:08 -0700

    fixing json read

--- server/server.py
+++ server/server.py
@@ -1,19 +1,24 @@
 import falcon
 import json
 from collections import namedtuple
-
+import random
+import redis
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 Unit = namedtuple('Unit', ['x', 'y'])
 unit = Unit(0,0)
 
 def get_num_at_end_of_branch(branch):
     if branch % 2 == 0:
-        x = branch/2
+        x = branch//2
         return (x) * (x+1)
     else:
-        x = (branch-1)/2
-        return (x) * (x+1) + (branch + 1)/2
+        x = branch//2
+        return (x) * (x+1) + (branch+1)//2
 
 def get_curve_position(x, y):
+    """
+    given an x and y, gives you the integer location on the space filling curve
+    """
     ya = int(abs(y))
     xa = int(abs(x))
     horizontal = False
@@ -43,13 +48,35 @@ def get_curve_position(x, y):
     elif vertical and x > 0:
         total_num = last_corner + (x-y)
     elif vertical and x < 0:
-        total_num = last_corner + xa + y 
+        total_num = last_corner + xa + y
+    elif x == y == 0:
+        return 0
     return total_num
 
+def get_x_y_position(curve_pos):
+    """
+    gets your position given your position on the curve
+    """
+    
+
+level_array = {}
+def generate_map(x, y):
+    for i in range(x):
+        for j in range(y):
+            z = random.randrange(0, 10)
+            if any([level_array.get(get_curve_position(i+a, j+b)) for a in range(-1, 2) for b in range(-1, 2)]):
+                if z <=5:
+                    z = 1
+            if z >= 3:
+                z = 3
+            level_array[get_curve_position(i, j)] = z
+    print(level_array)
+            #r.set(get_curve_position(i, j), z)
+            
 class MoveUnitResource:
     def on_post(self, req, resp):
         """Handles post requests"""
-        req_json = json.loads(req.stream.read())
+        req_json = json.loads(str(req.stream.read()))
         direction = req_json['direction']
         if direction == 0:
             unit.x += 1
@@ -66,6 +93,6 @@ class GetGridResource:
     def on_get(self, req, resp):
         resp.body = json.dumps({'units': [(unit.x, unit.y)]})
 
-#api = falcon.API()
-#api.add_route('/unit/move', MoveUnitResource())
-#api.add_route('/grid', GetGridResource())
+api = falcon.API()
+api.add_route('/unit/move', MoveUnitResource())
+api.add_route('/grid', GetGridResource())
