fa5f14da3197544fe2f056a5752025bc2fdebbae
thabaptiser <baptiste.vauthey@gmail.com>
2016-05-15 21:51:36 -0700

    small fixes

--- server/server.py
+++ server/server.py
@@ -12,39 +12,39 @@ class Unit():
         self.id = id
 
 def add_unit(max_id):
-    units[max_id] = Unit(0, 0, max_id)
+    s.units[max_id] = Unit(0, 0, max_id)
     return max_id + 1
 
 class ServerClass():
     max_id = 0
     units = {}
 
-    class MoveUnitResource:
-        def on_post(self, req, resp):
-            """Handles post requests"""
-            req_json = json.loads(req.stream.read().decode('utf-8'))
-            direction = req_json['direction']
-            id = req_json['id']
-            if direction == 0:
-                units[id].y += 1
-            elif direction == 1:
-                units[id].x += 1
-            elif direction == 2:
-                units[id].y -= 1
-            elif direction == 3:
-                units[id].x -= 1
-            resp.status = falcon.HTTP_200
+class MoveUnitResource:
+    def on_post(self, req, resp):
+        """Handles post requests"""
+        req_json = json.loads(req.stream.read().decode('utf-8'))
+        direction = req_json['direction']
+        id = req_json['id']
+        if direction == 0:
+            s.units[id].y += 1
+        elif direction == 1:
+            s.units[id].x += 1
+        elif direction == 2:
+            s.units[id].y -= 1
+        elif direction == 3:
+            s.units[id].x -= 1
+        resp.status = falcon.HTTP_200
 
-    class CreateUnitResource:
-        def on_get(self, req, resp):
-            max_id = add_unit(max_id)
+class CreateUnitResource:
+    def on_get(self, req, resp):
+        s.max_id = add_unit(s.max_id)
 
-    class GetGridResource:
-        def on_get(self, req, resp):
-            resp.body = json.dumps({'units': {str(id): (units[id].x, units[id].y) for id in units}})
+class GetGridResource:
+    def on_get(self, req, resp):
+        resp.body = json.dumps({'units': {str(id): (units[id].x, units[id].y) for id in s.units}})
 
 api = falcon.API()
 s = ServerClass()
-api.add_route('/unit/move', s.MoveUnitResource())
-api.add_route('/unit/create', s.CreateUnitResource())
-api.add_route('/grid', s.GetGridResource())
+api.add_route('/unit/move', MoveUnitResource())
+api.add_route('/unit/create', CreateUnitResource())
+api.add_route('/grid', GetGridResource())
