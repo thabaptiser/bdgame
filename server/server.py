bf209624be49865d8456bb3f12b07f351c40053f
thabaptiser <baptiste.vauthey@gmail.com>
2016-06-14 21:59:22 -0700

    small fixes

--- server/server.py
+++ server/server.py
@@ -40,8 +40,7 @@ class GetGridResource:
         ret = []
         for x in range(req_json['screen'][0][0], req_json['screen'][0][1]):
             for y in range(req_json['screen'][0][1], req_json['screen'][1][1]):
-                if s.units.get((x, y)):
-                    raise Exception((x, y))
+                #if s.units.get((x, y)):
                     ret.append((x, y))
         resp.body = json.dumps({'soldiers': ret})
     
