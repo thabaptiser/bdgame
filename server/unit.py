d57d926ea18dee58ab617f77e462e916bfbed51d
thabaptiser <baptiste.vauthey@gmail.com>
2017-01-09 20:56:51 -0800

    formatting

--- server/unit.py
+++ server/unit.py
@@ -1,11 +1,10 @@
-
 class Unit():
     def __init__(self, x, y, token, id):
         self.x = x
         self.y = y
         self.token = token
         self.life = 2
-        self.dest = (0,0)
+        self.dest = (0, 0)
         self.id = id
         self.moving = False
 
@@ -30,22 +29,22 @@ class Unit():
         if direction == 0:
             self.y += 1
             del s.units[(self.x, self.y)]
-            s.units[(self.x, self.y+1)] = self
+            s.units[(self.x, self.y + 1)] = self
 
         elif direction == 1:
             self.x += 1
             del s.units[(self.x, self.y)]
-            s.units[(self.x+1, self.y)] = self
+            s.units[(self.x + 1, self.y)] = self
 
         elif direction == 2:
             self.y -= 1
             del s.units[(self.x, self.y)]
-            s.units[(self.x, self.y-1)] = self
+            s.units[(self.x, self.y - 1)] = self
 
         elif direction == 3:
             self.x -= 1
             del s.units[(self.x, self.y)]
-            s.units[(self.x-1, self.y)] = self
+            s.units[(self.x - 1, self.y)] = self
 
         if self.x == direction[0] and self.y == direction[1]:
             self.moving = False
