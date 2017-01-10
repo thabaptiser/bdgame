2cfc51136da6b26f26a7e42c289b17564f9255f6
thabaptiser <baptiste.vauthey@gmail.com>
2017-01-09 22:35:59 -0800

    fixing something

--- server/server.py
+++ server/server.py
@@ -20,7 +20,7 @@ class ServerClass:
                 if self.units[u].moving:
                     self.units_coords[(self.units[u].x,
                                        self.units[u].y)] = None
-                    self.units[u].move()
+                    self.units[u].move(self)
                     self.units_coords[(self.units[u].x,
                                        self.units[u].y)] = self.units[u]
                 if self.units[u].dead:
